02f2acaa92403e7a4f9d594261986debef3ac5be
zopaj63 <zoran.pajnic@gmail.com>
2021-10-24 19:44:41 +0200

    add Clicker function with useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function Greeting(props) {
   console.log(props);
   return <div>{props.greeting} {props.children}!</div>
+}
+
+function Clicker() {
+  const [counter, setCounter] = useState(0);
+  const updateCounter = () => {
+    // setCounter(counter + 1);
+    // ili, bolja alternativa:
+    setCounter((state) => state + 1);
+  }
+  return <button onClick={updateCounter}>Clicked {counter} time(s)</button>;
 }
 
 function App() {
@@ -13,6 +24,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 
         <Greeting greeting="Hi">everybody</Greeting>
+
+        <Clicker />
+
 
         <a
           className="App-link"
