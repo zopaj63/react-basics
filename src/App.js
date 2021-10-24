44ab7d5fc17caf9d8fa9b8103af881435d3b23c0
zopaj63 <zoran.pajnic@gmail.com>
2021-10-24 20:15:07 +0200

    add TvShowList api request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 
@@ -17,16 +17,41 @@ function Clicker() {
   return <button onClick={updateCounter}>Clicked {counter} time(s)</button>;
 }
 
+//https://api.tvmaze.com/search/shows?q=arrested
+function TvShowList() {
+  useEffect(() => {
+    // console.log("I am rendered!");
+    fetch("https://api.tvmaze.com/search/shows?q=arrested")
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <div>Game of Thrones</div>
+      <div>Deadwood</div>
+      <div>Friday Night Dinner</div>
+    </div>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
+        <p className="description">Greeting function with props:</p>
         <Greeting greeting="Hi">everybody</Greeting>
 
+        <p className="description">Clicker function with useState:</p>
         <Clicker />
 
+        <p className="description">API fetch request with useEffect:"</p>
+        <TvShowList />
+
+        <p className="description">Class function:</p>
 
         <a
           className="App-link"
